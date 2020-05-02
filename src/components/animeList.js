0b983291888572b9691b287c1a527033d41ce6c0
Jon Haase <haase1020@gmail.com>
2020-05-02 19:16:07 -0400

    add styling

--- src/components/animeList.js
+++ src/components/animeList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Image, Box, Flex, Text } from "@chakra-ui/core";
 //can also use import { connect } from 'redux;
 // PhotosList = React.createClass({  })
 // PhotosList = connect(mapState, mapDispatch)(PhotosList)
@@ -8,14 +9,30 @@ import { useSelector } from "react-redux";
 //mapState takes the ste and returns props to be used by the component
 const Episode = ({ episode }) => {
   return (
-    <div>
-      <h3>{episode.title}</h3>
-      <img src={episode.image_url} alt="movie images" />
-      <p>{episode.synopsis}</p>
+  <Flex alignItems="center"
+  justifyContent="center"
+  >
+    <Box
+    maxW="md"
+    borderWidth="1px"
+    rounded="1g"
+    overflow="hidden"
+    margin="2%"
+    backgroundColor="#e7ecf0"
+    >
+      <Text fontSize="30px" as="u">{episode.title}</Text>
+      <Image
+      rounded="full"
+      size= "100px"
+      margin="2%"
+       src={episode.image_url} alt="movie images" />
+      <Text as="em"
+      >{episode.synopsis}</Text>
       <button>
         <a href={episode.url}>See More</a>
       </button>
-    </div>
+    </Box>
+  </Flex>
   );
 };
 
